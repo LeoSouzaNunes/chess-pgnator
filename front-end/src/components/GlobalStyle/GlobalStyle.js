119ff3894ad99c0192ee6b4359500e34ed514e2b
Leonardo de Souza Nunes <contato.leonunez@gmail.com>
2022-08-18 21:37:25 -0300

    style: add media queries

--- front-end/src/components/GlobalStyle/GlobalStyle.js
+++ front-end/src/components/GlobalStyle/GlobalStyle.js
@@ -49,4 +49,19 @@ b {
 
 `;
 
+const size = {
+    tablet: "768px",
+    laptop: "1024px",
+    laptopL: "1440px",
+    desktop: "2560px",
+};
+
+export const device = {
+    tablet: `(min-width: ${size.tablet})`,
+    laptop: `(min-width: ${size.laptop})`,
+    laptopL: `(min-width: ${size.laptopL})`,
+    desktop: `(min-width: ${size.desktop})`,
+    desktopL: `(min-width: ${size.desktop})`,
+};
+
 export default GlobalStyle;
